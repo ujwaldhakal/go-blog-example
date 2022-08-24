4e88979bae3cd2599f1891ca363c627e88dada79
Ujwal Dhakal <ujwal.dhakal@werkspot.nl>
2022-08-24 20:30:01 +0200

    Fixed database host name

--- db/connection.go
+++ db/connection.go
@@ -7,7 +7,7 @@ import (
 
 func GetConnection() *gorm.DB {
 	db, _ := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=127.0.0.1 user=postgres password=postgres dbname=postgres port=5432 sslmode=disable",
+		DSN:                  "host=pgsql user=postgres password=postgres dbname=postgres port=5432 sslmode=disable",
 		PreferSimpleProtocol: true, // disables implicit prepared statement usage
 	}), &gorm.Config{})
 
